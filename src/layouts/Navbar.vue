0d6f2cc177ad286fe96e2fb54fcd2fb3ed97329e
bendamqui <bendamqui@icloud.com>
2020-12-26 14:18:38 -0500

    test auto-update

--- src/layouts/Navbar.vue
+++ src/layouts/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
   <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0">
-    <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">SrVoc</a>
+    <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">SrVoc v0.1.4</a>
     <ul class="navbar-nav nav-actions px-3">
       <li class="nav-item">
         <b-button
