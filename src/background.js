b2a958acd075eb062ded840d17b81a2ab41046d8
bendamqui <bendamqui@icloud.com>
2020-12-26 10:52:50 -0500

    Set up auto update

--- src/background.js
+++ src/background.js
@@ -6,6 +6,7 @@ import { app, protocol, BrowserWindow } from "electron";
 import { createProtocol } from "vue-cli-plugin-electron-builder/lib";
 import installExtension, { VUEJS_DEVTOOLS } from "electron-devtools-installer";
 const isDevelopment = process.env.NODE_ENV !== "production";
+import { autoUpdater } from "electron-updater";
 
 // Scheme must be registered before the app is ready
 protocol.registerSchemesAsPrivileged([
@@ -35,6 +36,7 @@ async function createWindow() {
     createProtocol("app");
     // Load the index.html when not in development
     win.loadURL("app://./index.html");
+    autoUpdater.checkForUpdatesAndNotify();
   }
 }
 
